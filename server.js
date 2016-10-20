56e1c8e18ce6f726d81045afa6da0b950342a325
Elder <lopez_felix_23@hotmail.com>
2016-10-20 12:20:21 -0500

    Se actualizo la estructura

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const express = require('express');  
+const app = express();  
+const server = require('http').createServer(app);  
+const io = require('socket.io')(server);
 
+const bodyParser = require('body-parser');
+
+//Levantamos el servidor.......
 server.listen(process.env.PORT || 9000);
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json 
+app.use(bodyParser.json())
+
+app.use('/static', express.static(__dirname + '/public'));
 
 
 // Ahora necesitamos que el servidor de websockets, 
 //que lo tenemos en la variable io, esté atento a que se realice una conexión. 
 //Eso lo logramos con io.on() y pasándole el mensaje connection. 
+
 
 io.on('connection', function (socket) {
   socket.emit('messagesFront', { Messaje: 'Conexion Correcta A servidor NodeJs' });//Se envia el mensaje a el cliente
@@ -17,6 +30,9 @@ io.on('connection', function (socket) {
   });
 
 });
+// End  websockets
+
+
 
 
 
