5b881405f9e18a3d7ef223a3de806da8b5192d58
Elder <lopez_felix_23@hotmail.com>
2016-10-24 10:22:03 -0500

    Se crearon los services para el api, y se instala el pg para la conexion a la base de datos

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const app = express();
 const server = require('http').createServer(app);  
 const io = require('socket.io')(server);
 
+const pg = require('pg');
+
 const bodyParser = require('body-parser');
 
 //Levantamos el servidor.......
 server.listen(process.env.PORT || 9000);
+
+
+
+
+//var client = new pg.Client(config);
+
+
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,6 +25,28 @@ app.use(bodyParser.json())
 
 app.use('/static', express.static(__dirname + '/public'));
 
+// ********************************* Api rest *********************************
+app.get('/api/comment', (req, res)=> {
+  res.send(200,{comment:[]});
+});
+
+app.get('/api/comment/:commentId', (req, res)=> {
+  
+});
+
+app.post('/api/comment', (req, res)=> {
+	console.log(req.body);
+  	res.status(200).send({Messaje:'Comentario Ok'});
+});
+
+app.put('/api/comment/:commentId', (req, res)=> {
+  
+});
+
+app.delete('/api/comment/:commentId', (req, res)=> {
+  
+});
+// ********************************* Api rest *********************************
 
 // Ahora necesitamos que el servidor de websockets, 
 //que lo tenemos en la variable io, esté atento a que se realice una conexión. 
