960372aeeabd1d9b4e4050d900d53b4fa87790b6
EDGAR HERNANDEZ GARCIA <iscehg@gmail.com>
2017-01-20 17:58:25 -0600

    Libreria de Pitagoras

---
+++ controllers/teoremaPitagoras.js
@@ -0,0 +1,27 @@
+var operacion = require('./../lib/teoremaPitagoras');
+
+//POST - Calcula la operacion
+exports.calcular = function(req, res) {
+    console.log(req.body);
+    var respuesta;
+
+    var calPitagoras = new operacion.TeoremaPitagoras(req.body);
+    var resultado = calPitagoras.tipoCalculo();
+
+    if(isNaN(resultado)){
+        respuesta = {
+            codeE : 1,
+            jsonRespuesta :resultado,
+            mensajes : "No tiene solucion. Los valores de los catetos debe ser menor o igual a la hipotenusa"
+        };
+
+    }else{
+        respuesta = {
+            codeE : 0,
+            jsonRespuesta : resultado,
+            mensajes : "Calculo correcto"
+        };
+    }
+
+    res.status(200).jsonp(respuesta);
+};
