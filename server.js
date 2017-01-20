b964fabd299ed8edffb4bbf98b8c9cf381f1bb83
EDGAR HERNANDEZ GARCIA <iscehg@gmail.com>
2017-01-20 14:08:56 -0600

    Integrando APi de operaciones Matematicas

--- server.js
+++ server.js
@@ -1,72 +1,30 @@
-const express = require('express');  
-const app = express();  
-const server = require('http').createServer(app);  
-const io = require('socket.io')(server);
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require("method-override");
+var app = express();
 
-const pg = require('pg');
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
 
-const bodyParser = require('body-parser');
+// Import Models and Controllers
+var CtrlTeoremaPitagoras =require('./controllers/teoremaPitagoras');
+var router = express.Router();
 
-//Levantamos el servidor.......
-server.listen(process.env.PORT || 9000);
-
-
-
-
-//var client = new pg.Client(config);
-
-
-
-// parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json 
-app.use(bodyParser.json())
-
-app.use('/static', express.static(__dirname + '/public'));
-
-// ********************************* Api rest *********************************
-app.get('/api/comment', (req, res)=> {
-  res.send(200,{comment:[]});
+// Index - Route
+router.get('/', function(req, res) {  
+   res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/api/comment/:commentId', (req, res)=> {
-  
-});
+app.use(router);
 
-app.post('/api/comment', (req, res)=> {
-	console.log(req.body);
-  	res.status(200).send({Messaje:'Comentario Ok'});
-});
+// API routes
+var api = express.Router();
+api.route('/operaciones/teorema-pitagoras').post(CtrlTeoremaPitagoras.calcular);
+app.use('/api', api);
 
-app.put('/api/comment/:commentId', (req, res)=> {
-  
-});
-
-app.delete('/api/comment/:commentId', (req, res)=> {
-  
-});
-// ********************************* Api rest *********************************
-
-// Ahora necesitamos que el servidor de websockets, 
-//que lo tenemos en la variable io, esté atento a que se realice una conexión. 
-//Eso lo logramos con io.on() y pasándole el mensaje connection. 
-
-
-io.on('connection', function (socket) {
-  socket.emit('messagesFront', { Messaje: 'Conexion Correcta A servidor NodeJs' });//Se envia el mensaje a el cliente
-
-  socket.on('NewConection', function (data) {//Se recibe el mensaje de el cliente
-    console.log(data);
-  });
-
-});
-// End  websockets
-
-
-
-
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+// Start server
+app.listen(9000, function() {
+  console.log("Node server running on http://localhost:9000");
 });
